6c13a8566654dc66a45147e686e197336ea0d847
Claudiozzo01 <pellegriticlaudio01@gmail.com>
2026-05-04 15:30:55 +0200

    Add files via upload

---
+++ macellaio (1).py
@@ -0,0 +1,29 @@
+fila = ["Mario", "Giulia", "Tonino", "Rosa"]
+
+print("Persone in fila:", len(fila))
+
+cliente = fila.pop(0)
+print("Macellaio: Avanti", cliente + "!")
+print("Persone in fila:", len(fila))
+
+cliente = fila.pop(0)
+print("Macellaio: Avanti", cliente + "!")
+print("Persone in fila:", len(fila))
+
+fila.append("Enzo")
+print("Enzo si mette in fila")
+print("Persone in fila:", len(fila))
+
+cliente = fila.pop(0)
+print("Macellaio: Avanti", cliente + "!")
+print("Persone in fila:", len(fila))
+
+cliente = fila.pop(0)
+print("Macellaio: Avanti", cliente + "!")
+print("Persone in fila:", len(fila))
+
+cliente = fila.pop(0)
+print("Macellaio: Avanti", cliente + "!")
+print("Persone in fila:", len(fila))
+
+print("Negozio chiuso, a domani!")
