790da8b823bc6ecdd87bc1beb0a7b892a9fa23dc
Claudiozzo01 <pellegriticlaudio01@gmail.com>
2026-05-04 15:52:30 +0200

    Update macellaio (1).py

--- macellaio (1).py
+++ macellaio (1).py
@@ -1,12 +1,14 @@
-fila = ["Mario", "Giulia", "Tonino", "Rosa"]
+from collections import deque
+
+fila = deque(["Mario", "Giulia", "Tonino", "Rosa"])
 
 print("Persone in fila:", len(fila))
 
-cliente = fila.pop(0)
+cliente = fila.popleft()
 print("Macellaio: Avanti", cliente + "!")
 print("Persone in fila:", len(fila))
 
-cliente = fila.pop(0)
+cliente = fila.popleft()
 print("Macellaio: Avanti", cliente + "!")
 print("Persone in fila:", len(fila))
 
@@ -14,15 +16,15 @@ fila.append("Enzo")
 print("Enzo si mette in fila")
 print("Persone in fila:", len(fila))
 
-cliente = fila.pop(0)
+cliente = fila.popleft()
 print("Macellaio: Avanti", cliente + "!")
 print("Persone in fila:", len(fila))
 
-cliente = fila.pop(0)
+cliente = fila.popleft()
 print("Macellaio: Avanti", cliente + "!")
 print("Persone in fila:", len(fila))
 
-cliente = fila.pop(0)
+cliente = fila.popleft()
 print("Macellaio: Avanti", cliente + "!")
 print("Persone in fila:", len(fila))
 
